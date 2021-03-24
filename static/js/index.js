cede81bafbb29757a40fa99a2ec81d470bcde79d
FabianN98 <80277063+FabianN98@users.noreply.github.com>
2021-03-24 09:50:46 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,29 @@
 //https://www.eclipse.org/paho/clients/js/
 
-function LED1_On() {
+function LED1() {
+	if LED1.value ()==1:
+	LED1.on()
 	//alert("led on");
 	console.log("led on");
 	//document.getElementById("sensor").innerHTML="led on";
 	message = new Paho.MQTT.Message("ON");
         message.destinationName = "israelnoriega1998@hotmail.com/test1";
         client.send(message);
-  
-}
-function LED1_Off(){	
-	alert("led off");
+	else:
+	LED1.off()
+	//alert("led off");
 	console.log("led off");
 	//document.getElementById("sensor").innerHTML="led off";
 	message = new Paho.MQTT.Message("OFF");
         message.destinationName = "israelnoriega1998@hotmail.com/test1";
         client.send(message);
-}
 
+  
+}
+	
+	
+	
+	
 
 
 
